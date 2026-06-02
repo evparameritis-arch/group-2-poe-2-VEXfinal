5b319a2275a6e3100e991e95f1737c44ea60056b
evparameritis-arch <evparameritis@gmail.com>
2026-06-01 21:20:15 -0400

    Added vex project to repo

---
+++ g2-p2-VEXtransport/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       evpar                                                        #
+# 	Created:      6/1/2026, 9:19:55 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
